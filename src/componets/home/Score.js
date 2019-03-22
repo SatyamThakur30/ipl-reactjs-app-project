de45264c073abcfd54d4529a669f40efb3237f07
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-03-22 12:33:28 +0530

    Update Score.js

--- src/componets/home/Score.js
+++ src/componets/home/Score.js
@@ -12,7 +12,7 @@ export default class score extends Component {
   try {
     const data = await fetch(this.state.url);
     const jsonData = await data.json();
-    const filterData = jsonData.data.filter((item)=>item.unique_id ==1144173);
+    const filterData = jsonData.data.filter((item)=>item.unique_id ==114413);
     this.setState({
       liveData:filterData
     })
