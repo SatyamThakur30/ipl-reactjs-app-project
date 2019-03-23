ad00b888bcfd6863fd50f895649b39af651e6948
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-03-23 23:12:16 +0530

    Update capholderdata.js

--- src/componets/home/capholderdata.js
+++ src/componets/home/capholderdata.js
@@ -3,21 +3,21 @@ const capHolder = [
         id:1,
         capimage:"./capholder/orange-cap.png",
         title:"orange cap",
-        image:"./capholder/kane.jpg",
-        name:"Kane williamson",
-        run:"Run : 735",
-        matches:"Matches: 17",
-        team:"SRH"
+        image:"https://www.royalchallengers.com/sites/default/files/styles/large/public/team/2018ProfileLarge/2018PhotoshootLarge/Parthiv_Patel_213X280.jpg?itok=aaNhyL71",
+        name:"Parthiv Patel",
+        run:"Run : 29",
+        matches:"Matches: 1",
+        team:"RCB"
     },
     {
         id:2,
         capimage:"./capholder/purple-cap.png",
         title:"purple cap",
-        image:"./capholder/andrew.jpg",
-        name:"Andrew Tie",
-        wicket:"wicket : 24",
-        matches:"MAtches: 14",
-        team:"KXIP"
+        image:"https://media0.mensxp.com/media/content/2018/Apr/tahir-run-embed1-1524691053.jpg",
+        name:"Imran Tahir",
+        wicket:"wicket : 3",
+        matches:"Matches: 1",
+        team:"CSK"
     }
 ]
 export default capHolder;
