d1e989f7285d6fa678535258ee801967156f3ccd
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-03-24 20:01:19 +0530

    Update Pointsdata.js

--- src/componets/home/Pointsdata.js
+++ src/componets/home/Pointsdata.js
@@ -29,11 +29,11 @@ const pointstable = [
     {
         team:"SRH",
         logo:"./teamslogo/srh.png",
-        played:0,
+        played:1,
         won:0,
-        loss:0,
+        loss:1,
         points:0,
-        rr:0
+        rr:"-255"
     },
     {
         team:"RR",
@@ -47,11 +47,11 @@ const pointstable = [
     {
         team:"KKR",
         logo:"./teamslogo/kkr.png",
-        played:0,
-        won:0,
+        played:1,
+        won:1,
         loss:0,
-        points:0,
-        rr:0
+        points:2,
+        rr:"+255"
     },
     {
         team:"DC",
