9272f78f3ca97e2c8f4700ee1676b4641580d99a
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-04-04 23:22:30 +0530

    Update Fixturesdata.js

--- src/componets/home/Fixturesdata.js
+++ src/componets/home/Fixturesdata.js
@@ -133,14 +133,14 @@
       date:"Wednesday 3rd April",
       timing:"20:00 IST(14:30 GMT)",
       venue:"Wankhede",
-      winner:"MI-",by:"37 Runs"   },
+      winner:"MI",by:"37 Runs"   },
    {
       id:16,
       teams:"DC VS SRH",
       date:"Thursday 4th April",
       timing:" 20:00 IST(14:30 GMT)",
       venue:"Delhi",
-      winner:"-",by:"-"
+      winner:"SRH",by:"5 Wickets"
    },
    {
       id:17,
