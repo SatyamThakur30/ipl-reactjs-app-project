9f3aa1e4a1db685c27b44b3606d1e5ee0f196f80
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-04-22 20:00:09 +0530

    Update Nextmatch.js

--- src/componets/home/Nextmatch.js
+++ src/componets/home/Nextmatch.js
@@ -10,7 +10,7 @@ export default class Nextmatch extends Component {
    }
    async componentDidMount(){
     try {
-        const match1 = await this.state.data.filter((item)=>item.id ==40);
+        const match1 = await this.state.data.filter((item)=>item.id ==41);
        
         this.setState({
             data:match1
