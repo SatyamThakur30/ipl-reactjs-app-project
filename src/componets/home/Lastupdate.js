811086de307d0d9ae72dd1bc928639061de78858
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-04-26 23:40:50 +0530

    Update Lastupdate.js

--- src/componets/home/Lastupdate.js
+++ src/componets/home/Lastupdate.js
@@ -7,7 +7,7 @@ export default class Lastupdate extends Component {
     }
     async componentDidMount(){
         try {
-            const winner = await this.state.data.filter((item)=>item.id ==43);
+            const winner = await this.state.data.filter((item)=>item.id ==44);
            
             this.setState({
                 data:winner
