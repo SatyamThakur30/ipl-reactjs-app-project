0e543a7537d731c45d1aebdb06692dab632d0024
Satyam Thakur <46675749+SatyamThakur30@users.noreply.github.com>
2019-05-04 01:03:28 +0530

    Create playoffsdata.js

---
+++ src/componets/home/playoffsdata.js
@@ -0,0 +1,40 @@
+const playoffs=[
+    
+    {
+        id:1,
+        match:"Qualifier 1",
+        teams:"Team1 vs Team2",
+        date:"Tuesday 7th May",
+        timing:"7:30 IST(14:00 GMT)",
+        venue:" M. A. Chidambaram Stadium, Chennai",
+        winner:"-",by:"-"
+    },
+    {
+        id:2,
+        match:"Eliminator",
+        teams:"Team2 vs Team4",
+        date:"Wednesday 8th May",
+        timing:"7:30 IST(14:00 GMT)",
+        venue:"ACA-VDCA Stadium, Visakhapatnam",
+        winner:"-",by:"-"
+    },
+    {
+        id:3,
+        match:"Qualifier 2",
+        teams:"TBC vs TBC",
+        date:"Friday 10th May",
+        timing:"7:30 IST(14:00 GMT)",
+        venue:"ACA-VDCA Stadium, Visakhapatnam",
+        winner:"-",by:"-"
+    },
+    {
+        id:4,
+        match:"Final",
+        teams:"TBC vs TBC",
+        date:"Sunday 12th May",
+        timing:"7:30 IST(14:00 GMT)",
+        venue:"Rajiv Gandhi Intl. Cricket Stadium, Hyderabad",
+        winner:"-",by:"-"
+    }
+]
+export default playoffs;
